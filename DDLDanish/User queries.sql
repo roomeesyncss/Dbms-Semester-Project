12bd04cf5ba0cf0a707ba1813d731f1d421d906f
Danish <danishsidd524@gmail.com>
2024-06-04 21:45:08 +0500

    udpdate: queries

--- DDLDanish/User queries.sql
+++ DDLDanish/User queries.sql
@@ -2,6 +2,8 @@
 INSERT INTO [User] (Username, FullName, Email, Password)
 VALUES ('Rooma', 'Rooma Siddiqui', 'rooma@gmail.com', 'Abc12345%');
 
+-- SELECT * FROM [User]
+
 --Updating the user
 CREATE PROCEDURE UpdateUser
     @UserID INT,
@@ -30,25 +32,6 @@ END;
 
 --EXEC UpdateUser @UserID = '4', @Username = 'rooma', @FullName = 'Rooma Siddiqui', @Email = 'roomasiddiqui2003', @Password = 'Abc12345%', @Country = 'Pakistan', @Phone = '0023424234'
 
---Deleting the user
-CREATE PROCEDURE DeleteUser
-    @UserID VARCHAR(50)
-AS
-BEGIN
-    IF EXISTS (SELECT 1 FROM [User] WHERE UserID = @UserID)
-    BEGIN
-        DELETE FROM [User]
-        WHERE UserID = @UserID;
-
-        PRINT 'User deleted successfully.';
-    END
-    ELSE
-    BEGIN
-        PRINT 'User not found.';
-    END
-END
-
---EXEC DeleteUser @UserID = '1'
 
 -- -------- FRIENDSHIP QUERIES START -------------
 --Send Request
@@ -69,19 +52,19 @@ BEGIN
         RETURN;
     END
 
-    IF EXISTS (SELECT 1 FROM Friends WHERE (User1ID = @SenderID AND User2ID = @ReceiverID) OR (User1ID = @ReceiverID AND User2ID = @SenderID))
+    IF EXISTS (SELECT 1 FROM Friend WHERE (User1ID = @SenderID AND User2ID = @ReceiverID) OR (User1ID = @ReceiverID AND User2ID = @SenderID))
     BEGIN
         PRINT 'Error: Friendship already exists or request already sent.';
         RETURN;
     END
 
-    INSERT INTO Friends (User1ID, User2ID, FriendAddedDate)
+    INSERT INTO Friend (User1ID, User2ID, FriendAddedDate)
     VALUES (@SenderID, @ReceiverID, GETDATE());
 
     PRINT 'Friend request sent successfully.';
 END
 
-EXEC SendFriendRequest @SenderID = '2', @ReceiverID = '4'
+--EXEC SendFriendRequest @SenderID = 1, @ReceiverID = 2
 
 
 -- Accept/Reject Friend Request
@@ -91,7 +74,7 @@ CREATE PROCEDURE RespondToFriendRequest
     @Response NVARCHAR(20)
 AS
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM Friends WHERE User1ID = @SenderUserID AND User2ID = @ReceiverUserID AND Status = 'Pending')
+    IF NOT EXISTS (SELECT 1 FROM Friend WHERE User1ID = @SenderUserID AND User2ID = @ReceiverUserID AND Status = 'Pending')
     BEGIN
         PRINT 'Error: No request pending.';
         RETURN;
@@ -103,7 +86,7 @@ BEGIN
         RETURN;
     END
 
-    UPDATE Friends
+    UPDATE Friend
     SET Status = @Response
     WHERE User1ID = @SenderUserID AND User2ID = @ReceiverUserID AND Status = 'Pending';
 
@@ -117,7 +100,7 @@ BEGIN
     END
 END
 
-EXEC RespondToFriendRequest @SenderUserID = '2', @ReceiverUserID = '4', @Response = 'Accepted'
+--EXEC RespondToFriendRequest @SenderUserID = 1, @ReceiverUserID = 2, @Response = 'Accepted'
 
 
 -- Get Friends Of A User
@@ -126,20 +109,59 @@ CREATE PROCEDURE GetFriends
 AS
 BEGIN
     SELECT u.UserID, u.Username, u.Email, u.FullName, f.Status
-    FROM Friends f
+    FROM Friend f
     INNER JOIN [User] u ON (f.User1ID = u.UserID OR f.User2ID = u.UserID)
     WHERE (@UserID = f.User1ID OR @UserID = f.User2ID)
         AND f.Status = 'Accepted'
         AND u.UserID != @UserID;
 END;
 
+--EXEC GetFriends @UserID = 1
 
-EXEC GetFriends @UserID = 2
+-- Get Pending Requests
+CREATE PROCEDURE GetPendingFriendRequests
+    @UserID INT
+AS
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM [User] WHERE UserID = @UserID)
+    BEGIN
+        PRINT 'Error: User does not exist.';
+        RETURN;
+    END
+
+    SELECT f.User1ID AS SenderID, u.Username AS SenderUsername, u.FullName AS SenderFullName
+    FROM Friend f
+    INNER JOIN [User] u ON f.User1ID = u.UserID
+    WHERE f.User2ID = @UserID AND f.Status = 'Pending';
+    
+    PRINT 'Pending friend requests retrieved successfully.';
+END;
+
+--EXEC GetPendingFriendRequests @UserID = 2;
 
 -- -------- FRIENDSHIP QUERIES END -------------
 
 
 -- -------- NOTIFICATIONS QUERIES START -------------
+-- Get Notifications
+CREATE PROCEDURE GetNotifications
+    @UserID INT
+AS
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM [User] WHERE UserID = @UserID)
+    BEGIN
+        PRINT 'Error: User does not exist.';
+        RETURN;
+    END
 
+    SELECT NotificationID, Content, CreatedAt
+    FROM Notification
+    WHERE UserID = @UserID
+    ORDER BY CreatedAt DESC;
 
--- -------- NOTIFICATIONS QUERIES START -------------
+    PRINT 'Notifications retrieved successfully.';
+END;
+
+EXEC GetNotifications @UserID = 1
+
+-- -------- NOTIFICATIONS QUERIES END -------------
